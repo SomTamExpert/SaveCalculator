79dc2df6a88f78949bc5c5144b4968344b7900ef
Marco Karpf <2marco.karpf2@gmail.com>
2022-01-05 11:40:43 +0100

    feat(testing): add test-methods for substraction and addition

--- SaveCalculator/src/test/java/ch/bbw/km/savecalculator/AppTest.java
+++ SaveCalculator/src/test/java/ch/bbw/km/savecalculator/AppTest.java
@@ -9,31 +9,52 @@ import static org.junit.Assert.assertTrue;
  *  * @version 05.01.2022
  */
 public class AppTest {
-    /**
-     * Rigorous Test :-)
-     */
     @Test
-    public void testSummeZweiPositiveIsOk() {
+    public void testSummeZweiPositiveZahlenIsOk() {
         SaveCalculator testee = new SaveCalculator();
         int value1 = 10;
         int value2 = 20;
         assertTrue(testee.summarize(value1, value2) == 30);
     }
     @Test
-    public void testSummeGroesstePositiveZahlKleintePositiveZahlIsOk() {
+    public void testSummeGroesstePositiveZahlUndKleintePositiveZahlIsOk() {
         SaveCalculator testee = new SaveCalculator();
-        int value1 = 1;
-        int value2 =  2147483647;
+        int value1 = Integer.MAX_VALUE;
+        int value2 = 1;
         assertTrue(testee.summarize(value1, value2) == -2147483648);
     }
     @Test
-    public void testSubtraktionZweiPositiveIsOk() {
+    public void testSummeGroessteNegativeZahlUndKleinstePositiveZahlIsOk() {
+        SaveCalculator testee = new SaveCalculator();
+        int value1 = Integer.MIN_VALUE;
+        int value2 = 1;
+        assertTrue(testee.summarize(value1, value2) == -2147483647);
+    }
+    @Test
+    public void testSubtraktionZweiPositiveZahlenIsOk() {
         SaveCalculator testee = new SaveCalculator();
         int value1 = 10;
         int value2 = 20;
         assertTrue(testee.subtract(value1, value2) == -10);
     }
+    @Test
+    public void testSubtraktionGroessteNegativeZahlUndKleinsteNegativeZahlIsOk() {
+        SaveCalculator testee = new SaveCalculator();
+        int value1 = Integer.MIN_VALUE;
+        int value2 = -1;
+        assertTrue(testee.subtract(value1, value2) == 2147483647);
+    }
+    @Test
+    public void testSubtraktionGroesstePositiveZahlUndKleinsteNegativeZahlIsOk() {
+        SaveCalculator testee = new SaveCalculator();
+        int value1 = Integer.MAX_VALUE;
+        int value2 = -1;
+        assertTrue(testee.subtract(value1, value2) == -2147483648);
+    }
 }
+
+
+
 //tests to implement
 //eine positive + eine positive
 //eine negative + eine negative
