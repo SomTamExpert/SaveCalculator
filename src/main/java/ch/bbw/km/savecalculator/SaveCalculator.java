a3789d1e856bf68a7cdfe41778bf53e49eaff726
Marco Karpf <2marco.karpf2@gmail.com>
2022-01-24 11:38:12 +0100

    feat(calculations): add modulo method in savecalculator class, document calculation methods

--- src/main/java/ch/bbw/km/savecalculator/SaveCalculator.java
+++ src/main/java/ch/bbw/km/savecalculator/SaveCalculator.java
@@ -8,6 +8,7 @@ package ch.bbw.km.savecalculator;
  * @version 05.01.2022
  */
 public class SaveCalculator {
+	// summarize two integer
 	public int summarize(int summand1, int summand2) throws ArithmeticException
 	{
 		long value = (long) summand1 + (long) summand2;
@@ -16,6 +17,7 @@ public class SaveCalculator {
 		}
 		return summand1 + summand2;
 	}
+	// subtract two integer
 	public int subtract(int substrand1, int substrand2) throws  ArithmeticException
 	{
 		long value = (long) substrand1 - (long) substrand2;
@@ -24,6 +26,7 @@ public class SaveCalculator {
 		}
 		return substrand1 - substrand2;
 	}
+	// divide two integer
 	public int divide(int divid1, int divid2) throws ArithmeticException {
 		long value = (long) divid1 / (long) divid2;
 		if (divid2 == 0) {
@@ -31,6 +34,7 @@ public class SaveCalculator {
 		}
 		return divid1 / divid2;
 	}
+	// multiply two integer
 	protected int multiply(int multip1, int multip2) throws ArithmeticException{
 		long value = (long) multip1 * (long) multip2;
 		if((value > Integer.MAX_VALUE) || (value < Integer.MIN_VALUE)) {
@@ -38,20 +42,27 @@ public class SaveCalculator {
 		}
 		return multip1 * multip2;
 	}
+	// square two integer
 	private int square(int quadrant1, int quadrant2) throws ArithmeticException {
 		long value = (long) Math.pow((long) quadrant1, (long) quadrant2);
-		if((value > Integer.MAX_VALUE) || value <Integer.MIN_VALUE) {
+		if((value > Integer.MAX_VALUE) || (value < Integer.MIN_VALUE)) {
 			throw new ArithmeticException();
 		}
 		return (int) Math.pow(quadrant1, quadrant2);
 	}
+	// calculate square-root from an integer
 	public int squareRoot(int base) throws ArithmeticException {
 		long value = (long) Math.sqrt((long) base);
-		if ((value) > Integer.MAX_VALUE || (value < Integer.MIN_VALUE)) {
+		if ((value > Integer.MAX_VALUE) || (value < Integer.MIN_VALUE)) {
 			throw new ArithmeticException();
 		}
 		return (int) Math.sqrt(base);
 	}
+	// calculate modulo from two integer
+	public int modulo(int modulo1, int modulo2) {
+		return modulo1 % modulo2;
+	}
+	// resets the values,  used in AppTest
 	public void delete(int value1, int value2) {
 		value1 = 0;
 		value2 = 0;
